f481ad0ff3ec8454c8671b22ca1f5b10e8f5c01b
Elizabeth Durflinger <elizabeth@drund.com>
2019-03-19 19:10:33 -0400

    Adds login and registration routes

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET login listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const MongoClient = require('mongodb').MongoClient;
 
+// Connection URL
+const url = process.env.MONGO_DB_URL;
+// Database Name
+const dbName = 'deals-data';
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+/* GET login listing. */
+router.post('/', function(req, res, next) {
+    console.log(req.body)
+    // Use connect method to connect to the Server
+    client.connect(function(err) {
+      console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+
+        // Get the documents collection
+        const collection = db.collection('user-stuff');
+        // Find some documents
+        collection.findOne( {"email": req.body["email"], "password": req.body["password"]}, function(err, results) {
+            if(err || !results)
+                return res.sendStatus(403);
+            else
+                collection.find({'email' : req.body.email}).toArray(function(err, docs) {
+                  console.log("Found the following records");
+                  console.log(docs);
+                  response = docs;
+                  res.send(response);
+                });
+        });
+    });
+});
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = process.env.MONGO_DB_URL;
+// Database Name
+const dbName = 'deals-data';
+// Create a new MongoClient
+const client = new MongoClient(url);
+
 /* GET register listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+    console.log(req.body)
+    // Use connect method to connect to the Server
+    client.connect(function(err) {
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+
+        // Get the documents collection
+        const collection = db.collection('user-stuff');
+        // Find some documents
+        collection.insertOne(req.body);
+        
+        if(err)
+            return res.sendStatus(403);
+        else{
+            return res.sendStatus(200);
+        }
+    });
 });
 
 module.exports = router;
