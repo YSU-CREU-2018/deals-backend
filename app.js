52a9f27d1c533fbd153b980b6bacec89de849d50
Elizabeth Durflinger <elizabeth@drund.com>
2019-03-27 11:41:23 -0400

    cors - try to fix

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ var registerRouter = require('./routes/register');
 var app = express();
 app.use(cors());
 
-app.use(cors({
-    'orgin': *,
-    'preflightContinue': false,
-}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
