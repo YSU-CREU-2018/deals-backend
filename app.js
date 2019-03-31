8b9ba9f8f996eab33898eecb01fc5bd1ffaca56e
Elizabeth Durflinger <elizabeth@drund.com>
2019-03-31 17:45:29 -0400

    removing unused headers

--- app.js
+++ app.js
@@ -18,11 +18,6 @@ var app = express();
 app.use(cors());
 app.options('*', cors())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
