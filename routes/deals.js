f49b9805ccdbb6e7b210b4e284e4d204d9d71292
Elizabeth Durflinger <elizabeth@drund.com>
2019-04-24 13:06:24 -0400

    Deals get works

--- routes/deals.js
+++ routes/deals.js
@@ -1,35 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-
-/* GET deals listing. */
-const MongoClient = require('mongodb').MongoClient;
-
-// Connection URL
-const url = process.env.MONGO_DB_URL;
-// Database Name
-const dbName = 'deals-data';
-// Create a new MongoClient
-const client = new MongoClient(url);
-
 /* GET profile listing. */
 router.get('/', function(req, res, next) {
+
+    /* GET deals listing. */
+    const MongoClient = require('mongodb').MongoClient;
+
+    // Connection URL
+    const url = process.env.MONGO_DB_URL;
+    // Database Name
+    const dbName = 'deals-data';
+    // Create a new MongoClient
+    const client = new MongoClient(url);
 
     var response = '';
 
     // Use connect method to connect to the Server
-    client.connect(function(err) {
+    client.connect(function(err, client) {
+        const db = client.db(dbName);
+        // Get the documents collection
+        db.collection('recommender-site').aggregate(
+            [{ $sample: { size: 50} }],
+            function(err, response){
+                response.toArray(function(err, docs) {
+                    response = docs;
+                    client.close();
 
-      const db = client.db(dbName);
-      // Get the documents collection
-      const collection = db.collection('recommender-site');
-      // Find some documents
-      collection.aggregate([{ $sample: { size: 50} }]).toArray(function(err, docs) {
-        response = docs;
-        res.send(response);
-      });
+                    res.send(response);
+                });
+            }
+        )
     });
 });
+
 //will not need we cut this out but i am keeping it for safekeeping
 // router.get('/rated/:user_id', function(req, res, next) {
 //   res.send('respond with a resource');
