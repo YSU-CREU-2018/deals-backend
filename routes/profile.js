ada996b631482584e3486e75dc10a0ca7f3ff8b9
Elizabeth Durflinger <elizabeth@drund.com>
2019-04-24 13:39:12 -0400

    profile connection closing works

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-
-const MongoClient = require('mongodb').MongoClient;
-
-// Connection URL
-const url = process.env.MONGO_DB_URL;
-// Database Name
-const dbName = 'deals-data';
-// Create a new MongoClient
-const client = new MongoClient(url);
-
 /* GET profile listing. */
 router.post('/', function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient;
+
+    // Connection URL
+    const url = process.env.MONGO_DB_URL;
+    // Database Name
+    const dbName = 'deals-data';
+    // Create a new MongoClient
+    const client = new MongoClient(url);
+
     var response = '';
 
     // Use connect method to connect to the Server
@@ -21,17 +20,26 @@ router.post('/', function(req, res, next) {
         const db = client.db(dbName);
 
         // Get the documents collection
-        const collection = db.collection('user-stuff');
-        // Find some documents
-        collection.findOne( {"email": req.body["email"]}, function(err, results) {
-            if(err || !results)
-                return res.sendStatus(403);
-            else
-                collection.find({'email' : req.body.email}).toArray(function(err, docs) {
-                    response = docs;
-                    res.send(response);
-                });
-        });
+        db.collection('user-stuff').findOne(
+            {"email": req.body["email"]},
+            function(err, results) {
+                if(err || !results){
+                    client.close();
+                    return res.sendStatus(403);
+                }else{
+                    collection.find(
+                        {'email' : req.body.email},
+                        function(err, response){
+                            response.toArray(function(err, docs) {
+                                response = docs;
+                                client.close();
+                                res.send(response);
+                            });
+                        }
+                    )
+                }
+            }
+        );
     });
 });
 
