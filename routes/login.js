dbbfc37a2bb72a48f501f961b8f3a95a20ecbe84
Elizabeth Durflinger <elizabeth@drund.com>
2019-04-24 13:34:51 -0400

    Login connection close works

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-const MongoClient = require('mongodb').MongoClient;
-
-// Connection URL
-const url = process.env.MONGO_DB_URL;
-// Database Name
-const dbName = 'deals-data';
-// Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true });
-
 /* GET login listing. */
 router.post('/', function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient;
+
+    // Connection URL
+    const url = process.env.MONGO_DB_URL;
+    // Database Name
+    const dbName = 'deals-data';
+    // Create a new MongoClient
+    const client = new MongoClient(url, { useNewUrlParser: true });
+
     var response = '';
     // Use connect method to connect to the Server
     client.connect(function(err) {
@@ -21,18 +21,30 @@ router.post('/', function(req, res, next) {
         const db = client.db(dbName);
 
         // Get the documents collection
-        const collection = db.collection('user-stuff');
-        // Find some documents
-        collection.findOne( {"email": req.body["email"], "password": req.body["password"]}, function(err, results) {
-            if(err || !results)
-                return res.sendStatus(403);
-            else
-                collection.find({'email' : req.body.email}).toArray(function(err, docs) {
-                    response = docs;
-                    console.log(res);
-                    res.send(response);
-                });
-        });
+        db.collection('user-stuff').findOne(
+            {
+                "email": req.body["email"],
+                "password": req.body["password"]
+            },
+            function(err, results) {
+                if(err || !results){
+                    client.close();
+                    return res.sendStatus(403);
+                }else{
+                    collection.find(
+                        {'email' : req.body.email},
+                        function(err, response){
+                            response.toArray(function(err, docs) {
+                                response = docs;
+                                client.close();
+                                console.log(res);
+                                res.send(response);
+                            });
+                        }
+                    )
+                }
+            }
+        );
     });
 });
 module.exports = router;
