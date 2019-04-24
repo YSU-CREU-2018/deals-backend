ac9329e5f1588982003e73d029572e95dfd38885
Elizabeth Durflinger <elizabeth@drund.com>
2019-04-24 13:50:36 -0400

    reset password connection close is fixed

--- routes/forgot.js
+++ routes/forgot.js
@@ -1,38 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-const MongoClient = require('mongodb').MongoClient;
-
-// Connection URL
-const url = process.env.MONGO_DB_URL;
-// Database Name
-const dbName = 'deals-data';
-// Create a new MongoClient
-const client = new MongoClient(url);
-
 /* GET forgot listing. */
 router.post('/', function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient;
+
+    // Connection URL
+    const url = process.env.MONGO_DB_URL;
+    // Database Name
+    const dbName = 'deals-data';
+    // Create a new MongoClient
+    const client = new MongoClient(url);
+
     var response = '';
     client.connect(function(err) {
 
         const db = client.db(dbName);
-        // Get the documents collection
-        const collection = db.collection('user-stuff');
-
         const query = { "email" : req.body.email };
         const update = {
             "$set": {
                 "password": req.body["password"]
                 }
             };
-        // Find some documents
-        collection.findOne( {"email": req.body["email"]}, function(err, results) {
-            if(err || !results)
-                return res.sendStatus(403);
-            else
-                collection.updateOne(query, update);
-                return res.sendStatus(200);
-        });
+        // Get the documents collection
+        db.collection('user-stuff').findOne(
+            {"email": req.body["email"]},
+            function(err, results) {
+                if(err || !results){
+                    client.close();
+                    return res.sendStatus(403);
+                }else{
+                    collection.updateOne(
+                        query,
+                        update,
+                        function(err, response){
+                            client.close();
+                            return res.sendStatus(200);
+                        }
+                    );
+                }
+            }
+        );
     });
 });
 
